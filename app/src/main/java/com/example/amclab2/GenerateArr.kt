08c56bac5e97c9058496bbf5af3a8f247e993a2d
MazanYan <mazanyan027@gmail.com>
2019-03-09 21:20:14 +0200

    fix file input

--- app/src/main/java/com/example/amclab2/GenerateArr.kt
+++ app/src/main/java/com/example/amclab2/GenerateArr.kt
@@ -2,32 +2,10 @@ package com.example.amclab2
 
 import java.math.RoundingMode
 import java.text.DecimalFormat
-//
-//import java.io.BufferedReader;
-//import java.io.BufferedWriter;
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.io.FileWriter;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.io.OutputStreamWriter;
-//
-//import android.app.Activity;
-//import android.os.Bundle;
-//import android.os.Environment;
-//import android.util.Log;
-//import android.view.View;
-//
-//import android.support.v7.app.AppCompatActivity
-//import android.os.Bundle
-//import android.view.View
-//import android.view.ViewGroup
-//import kotlinx.android.synthetic.main.activity_main.*
-//import java.io.File
-//import java.io.FileInputStream
+import kotlin.random.Random
 
 /**
+ * @input Unit
  * @return 10 arrays of different size with random double numbers in range [0,1] rounded to 4 decimal points
  */
 fun generateArr() : Array<Array<Double>> {
@@ -35,18 +13,23 @@ fun generateArr() : Array<Array<Double>> {
     doubleFormat.roundingMode = RoundingMode.CEILING
     return Array(10, {i ->
         Array(5+i*i*5, {_ ->
-            doubleFormat.format(kotlin.random.Random.nextDouble()).toDouble()})})
+            doubleFormat.format((Random.nextDouble()-0.5)*200).toDouble()})})
 }
 
+
+/**
+ * @input string representation of a file with lines connected by \n
+ * @return 10 arrays of different size with random double numbers in range [0,1] rounded to 4 decimal points
+ */
 fun parseInputFromFile(src: String) : Array<Array<Double>> {
     val split: List<String> = src.split("\n").filter({i -> i != ""})
     val lines_length: Int = split.size
-    var comment_line_begin: Int = 0
-    var comment_line_end: Int = 0
+    val comment_line_begin: Int?
     if (split.contains("/*"))
         comment_line_begin = split.indexOf("/*")+1
     else
         comment_line_begin = split.size
+    //delete everything after comment sign
     val array: List<List<Double>> = split.dropLast(lines_length-comment_line_begin+1).map({ s -> s.split(" ").map { i -> i.toDouble() } })
     return array.map({e -> e.toTypedArray()}).toTypedArray()
 }
--- app/src/main/java/com/example/amclab2/MainActivity.kt
+++ app/src/main/java/com/example/amclab2/MainActivity.kt
@@ -5,41 +5,37 @@ import android.os.Bundle
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.View;
+import android.widget.Toast
 import java.io.*
-
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     var arrays: Array<Array<Double>> = Array(1,{_->Array(1, {_-> 0.0})})
-//    var generateState: Int = 0
+    var filePath: String? = null
+    val fileName: String = "AMC_Lab2.txt"
+    val baseFile: String = """
+-13.4 4 -10 0
+1 2 3 -8 5
+2 14 -13 26 145.5 16.04
 
-
-
+/*
+    EVERYTHING AFTER A COMMENT SIGN ISN'T READ
+    the input is a bunch of arrays with numbers delimited by single space. Each array starts from a new line
+    EG:
+-13.4 4 -10 0
+1 2 3 -8 5
+2 14 -13 26 145.5 16.04
+    """.trimIndent()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        filePath = getExternalFilesDir(null).absolutePath.toString()
+        pathToFile.text="$filePath/$fileName - path to data file to edit"
+        if (!File("$filePath/$fileName").exists())
+            initialFileCreate()
     }
-
-
-
-
-//    /**
-//     * state == 0 -> generates array
-//     * state == 1 -> sorts array
-//     */
-//    fun switchButtonState(view: View) {
-//        if (generateState.equals(0)) {
-//            generateState = 1
-//            button.text = "Відсортувати масиви"
-//            button.setOnClickListener { v -> generateSortedArr(v) }
-//        }
-//        else {
-//            generateState = 0
-//            button.text = "Згенерувати масиви"
-//            button.setOnClickListener { v -> generateArrOnClick(v) }
-//        }
-//    }
 
 
 
@@ -51,41 +47,39 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    fun initialFileCreate() {
+        Toast.makeText(this, "The initial file didn't exist before and has been automatically created", Toast.LENGTH_SHORT).show()
+        val initialFile: File = File(filePath+"/"+fileName)
+        PrintWriter(initialFile).use { out -> out.println(baseFile) }
+//        baseContext.openFileOutput(filePath+"/"+fileName, Context.MODE_PRIVATE).use {
+//            it.write((baseFile.toByteArray()))
+//        }
+    }
     fun generateFromFile(view: View) {
-        val filename: String = "/data/user/0/com.example.amclab2/files/AMC_Lab2.txt"
-
         try {
-            // открываем поток для чтения
-            val br = BufferedReader(
-                InputStreamReader(
-                    openFileInput(filename)
-                )
-            )
-            var str = ""
-            // читаем содержимое
-            while ((br.readLine()) != null) {
-                str+=br.readLine()
+            val sdPath = File(filePath + "/" + fileName)
+            if (!sdPath.exists())
+                initialFileCreate()
+            val stringSdcardPath: String = File(sdPath.absolutePath.toString().plus("/")).toString()
+            val file: File = File(stringSdcardPath)
+            try {
+                val reader = BufferedReader(FileReader(file))
+                var str = ""
+                var line: String? = ""
+                while (line != null) {
+                    line = reader.readLine()
+                    str += line.plus("\n")
+                }
+                this.arrays = parseInputFromFile(str)
+                showOnCanvas(view, parseForTextView(arrays))
+                button3.setOnClickListener({ v -> generateSortedArr(v) })
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
-            this.arrays = parseInputFromFile(str)
-            showOnCanvas(view,parseForTextView(arrays))
-//            var res = ""
-//            for (i in arrays) {
-//                var res_single: String = "["
-//                for (j in i)
-//                    res_single += "$j, "
-//                res += "${res_single.dropLast(2)}]\n\n"
-//            }
-//            textView.text = res
-//            val params: ViewGroup.LayoutParams = textView.getLayoutParams()
-//            params.height = 300
-//            textView.setLayoutParams(params)
-
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
         }
-
+        catch (e: FileNotFoundException) {
+            Toast.makeText(this, "The input file has been deleted!", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
@@ -95,7 +89,7 @@ class MainActivity : AppCompatActivity() {
         val generated: Array<Array<Double>> = generateArr()
         this.arrays = generated
         showOnCanvas(view,parseForTextView(arrays))
-//        switchButtonState(view)
+        button3.setOnClickListener({v -> generateSortedArr(v)})
     }
 
 
@@ -130,7 +124,6 @@ class MainActivity : AppCompatActivity() {
 
     fun showOnCanvas(view: View, text: String) {
         textView.text = text
-//        textView.height = 300
         val params: ViewGroup.LayoutParams = textView.getLayoutParams()
         params.height = 300
         textView.setLayoutParams(params)
